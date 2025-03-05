6a5106c33da98dcc8102e480e7e97f35480c9363
suria sultana <138383854+suraiyasultana826@users.noreply.github.com>
2025-03-05 19:06:20 +0600

    Add files via upload

---
+++ mapped.ts
@@ -0,0 +1,28 @@
+{
+    const arrOfNumbers: number[] = [1, 2, 3];
+    // const arrOfStrings: string[] = ['1', '2', '3']
+    const arrOfStrings: string[] = arrOfNumbers.map((number) => number.toString());
+    console.log(arrOfStrings);
+
+    type AreaNumber = {
+        height: number;
+        width: number;
+    };
+
+    type Height = AreaNumber["height"] //look up type
+
+    // type AreaString = {
+    //     height: string;
+    //     width: string 
+    // }
+
+
+
+    type AreaString<T> = {
+        [key in keyof T]: T[key]
+    }
+    const area1 : AreaString<{height : string ; width: number}> = {
+        height:'100',
+        width: 50
+    }
+}
